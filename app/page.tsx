93218e71ebc2af11e94bf7f914493024cb348746
danyulgh <daniel.g.hoang2007@gmail.com>
2025-12-16 00:40:35 -0500

    desk and wait.

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,33 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="flex min-h-screen items-center justify-center font-sans ">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between bg-black sm:items-start">
+        <div className = "flex justify-center items-end h-[70vh] bg-black w-full z-10">
+          <Image
+            src="/wait.png"
+            alt="wait for the next bell."
+            width={0}
+            height={0}
+            sizes="100vh"
+            className="object-contain"
+            style={{width: 'auto', height:'40vh'}}
+          />
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="-mt-[70vh] h-[170vh] w-full sticky ">
+          <div className="sticky top-0 min-h-screen flex items-center justify-center">
+            <Image
+              src="/desk.png"
+              alt="[here lies a desk]"
+              width={0}
+              height={0}
+              sizes="100vh"
+              className="object-contain absolute"
+              style={{width: 'auto', height:'50vh'}}
+            />
+            <div className="size-[25vw] bg-radial from-white via-zinc-800 to-black to-70%"/>
+          </div>
         </div>
       </main>
     </div>
